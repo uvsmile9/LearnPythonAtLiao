fe262feb7db92a686a812ff3b0a3e5a6ba26d860
uvsmile9_mac <uvsmile9@gmail.com>
2022-02-17 16:50:56 +0800

    a commit

--- def_func.py
+++ def_func.py
@@ -8,10 +8,12 @@ Created on Tue Jan 18 11:48:32 2022
 
 import math
 
-def quadratic(a,b,c):
-    x1=(-b+math.sqrt(b**2-4*a*c))/(2*a)
-    x2=(-b-math.sqrt(b**2-4*a*c))/(2*a)
-    return x1,x2
+
+def quadratic(a, b, c):
+    x1 = (-b+math.sqrt(b**2-4*a*c))/(2*a)
+    x2 = (-b-math.sqrt(b**2-4*a*c))/(2*a)
+    return x1, x2
+
 
 print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
 print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
